da9653839fa6273eb10efaa39d8b14992d47cb96
T Ciernik <tomas.ciernik@and.digital>
2023-12-01 11:03:58 +0000

    initial spring app

---
+++ src/kotlin-spring-api/studentSetvice/src/main/kotlin/com/kotlinDemoApi/studentSetvice/StudentSetviceApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinDemoApi.studentSetvice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentSetviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentSetviceApplication>(*args)
+}
---
+++ src/kotlin-spring-api/studentSetvice/src/test/kotlin/com/kotlinDemoApi/studentSetvice/StudentSetviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinDemoApi.studentSetvice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentSetviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
