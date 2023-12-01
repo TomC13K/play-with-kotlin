a40ac082da1af641ae46aada8327bf8040087817
T Ciernik <tomas.ciernik@and.digital>
2023-12-01 13:36:59 +0000

    fixed horendous JVM version issues and ammended test to pass

--- src/kotlin-spring-api/studentSetvice/src/test/kotlin/com/kotlinDemoApi/studentService/StudentRepositoryTest.kt
+++ src/kotlin-spring-api/studentSetvice/src/test/kotlin/com/kotlinDemoApi/studentService/StudentRepositoryTest.kt
@@ -3,6 +3,12 @@ package com.kotlinDemoApi.studentService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.assertj.core.api.Assertions;
+import org.mockito.ArgumentMatchers.any
+import org.mockito.BDDMockito.given
+
+import org.assertj.core.api.BDDAssertions.then
+
 
 @DataJpaTest
 class StudentRepositoryTest @Autowired constructor(
@@ -13,12 +19,15 @@ class StudentRepositoryTest @Autowired constructor(
     fun `test Student by Name & returns student details` () {
 
         //given
-        val student = Student(null,"Mark")
-        var savedStudent: Student = studentRepository.save(student)
+        val studentName = Student(null,"Mark")
+        var savedStudent: Student = studentRepository.save(studentName)
 
         //when
+        var student : Student = studentRepository.findByName("Mark")
 
         //then
+        then(student.id).isNotNull()
+        then(student.name).isEqualTo(savedStudent.name)
 
     }
 }
